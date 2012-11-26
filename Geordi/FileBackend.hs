287ce8f2ed1081d6233e3b784044a3499ec92857
Liam O'Connor <liamoc@cse.unsw.edu.au>
2012-11-27 08:48:58 +1100

    More interesting test program, no more null filebackend

--- Geordi/FileBackend.hs
+++ Geordi/FileBackend.hs
@@ -1,14 +1,8 @@
 module Geordi.FileBackend where
 import qualified Data.ByteString.Lazy as L
 import Network.Wai.Parse
-import Data.Conduit.Util
 
 newtype FileBackend a = FB (BackEnd a)
-
-nullBackend :: FileBackend a
-nullBackend = FB $ const $ const $ sinkState () 
-                         (\() _ -> return $ StateDone Nothing (error "Using undefined file backend")) 
-                         (const $ return $ error "Using undefined file backend") 
 
 lbsBackend :: FileBackend L.ByteString
 lbsBackend = FB $ lbsBackEnd
--- test.hs
+++ test.hs
@@ -4,5 +4,21 @@ import Geordi
 import Network.Wai.Handler.Warp
 import Debug.Trace
 import Data.Monoid
-
-main = geordi 3000 $ return ()
+import Network.HTTP.Types.Status
+import Control.Monad.Trans
+import Network.Wai
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Text as T
+import qualified Data.Map  as M
+main = geordi 3000 $ do
+         get (str "baz" // splat // str "foo" // splat // str "bing") $ \l r -> do
+           liftIO $ putStrLn $ "1: " ++ (concat l)
+           liftIO $ putStrLn $ "2: " ++ (concat r)
+           respond $ const $ responseLBS status404 [] $ L.pack (concat l)
+         get (str "baz" // splat // str "bar" // allQuery) $ \l (qs :: M.Map T.Text [String]) -> do
+           liftIO $ putStrLn $ show qs
+           respond $ const $ responseLBS status404 [] $ L.pack (concat l)
+         request splat $ \l -> do
+           liftIO $ putStrLn (concat l)
+           respond $ const $ responseLBS status404 [] "FOOBAR"
+         return ()
